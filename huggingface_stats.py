b72a6017ab065d2daa775802a95abf9ee443bca3
Erik <73839432+JakobGroenenhaag@users.noreply.github.com>
2022-06-16 13:41:05 +0200

    Logistic regression werkt soort van

--- huggingface_stats.py
+++ huggingface_stats.py
@@ -9,6 +9,7 @@ import itertools
 import re
 from collections import Counter
 from tqdm import tqdm
+import numpy as np
 
 class Preprocessor:
     """
@@ -112,3 +113,69 @@ class Preprocessor:
             for doc in docs:
                 freqs += self._document_frequency_helper(doc)
         return freqs
+
+class tf_idf_document_scorer:
+    """
+    Calculates a score for a document that represents how likely
+    the document fits the positive class, based on the average
+    tf-idf per class for each word
+
+    preprocessor: preprocessor object
+    docs: list of documents to train the scorer
+    labels: list of labels (1 or 0) that belong to the documents
+    show_progress: boolean, controls whether or not a progress bar shows
+    """
+    def __init__(self, preprocessor: Preprocessor, docs: list, labels: list, show_progress=False):
+        self._preprocessor = preprocessor
+
+        # learn the word weights
+        doc_freq = preprocessor.document_frequency(docs=docs, show_progress=show_progress)
+        vocab = list(dict(doc_freq.items()).keys())
+
+        docs_positive = np.array(docs)[np.array(labels) == 1].tolist()
+        docs_negative = np.array(docs)[np.array(labels) == 0].tolist()
+        summed_tf_positive = preprocessor.batch_word_count(docs=docs_positive, show_progress=show_progress)
+        summed_tf_negative = preprocessor.batch_word_count(docs=docs_negative, show_progress=show_progress)
+
+        N = len(docs)
+        n_positive = len(docs_positive)
+        n_negative = len(docs_negative)
+
+        self._weights = dict()
+        for word in vocab:
+            avg_tf_pos = summed_tf_positive[word] / n_positive
+            avg_tf_neg = summed_tf_negative[word] / n_negative
+            idf = np.log(N / (1 + doc_freq[word]))
+            self._weights[word] = (avg_tf_pos - avg_tf_neg) * idf
+
+    def score(self, doc: str):
+        """
+        Computes a score for a document, based on a bag-of-words approach.
+        The higher the score, the more it fits the positive documents in
+        the training data.
+        """
+        word_count = self._preprocessor.word_count(doc=doc)
+        score = 0
+        for word, count in word_count.items():
+            if word in self._weights:
+                score += count * self._weights[word]
+        return score
+
+    def batch_score(self, docs: list, show_progress=False):
+        """
+        returns a list of scores for the documents
+
+        docs: list of strings, documents to score
+        show_progress: boolean, controls whether or not a progress bar shows
+        """
+        scores = []
+        # score all docs with a visual progress bar
+        if show_progress:
+            for i in tqdm(range(len(docs))):
+                scores.append(self.score(docs[i]))
+        # score all docs without a visual progress bar
+        else:
+            for doc in docs:
+                scores.append(self.score(doc))
+
+        return scores
